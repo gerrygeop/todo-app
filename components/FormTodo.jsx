f97d260df127814318ba17831c988a72f3f01192
gerrygeop <gerryja28@gmail.com>
2023-04-25 10:08:31 +0800

    create todo :tada:

--- components/FormTodo.jsx
+++ components/FormTodo.jsx
@@ -1,33 +1,48 @@
 "use client";
 import Button from "./Button";
 import Input from "./Input";
+import Label from "./Label";
 
 export default function FormTodo() {
-	function handleOnSubmit(e) {
+	async function handleOnSubmit(e) {
 		e.preventDefault();
-		const formData = new FormData();
+		const formData = new FormData(e.target);
+		const res = await fetch("/api/todos", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				todo: formData.get("todo"),
+				date: formData.get("date"),
+				time: formData.get("time"),
+			}),
+		});
+
+		const data = await res.json();
+		console.log(data);
+		e.target.reset();
 	}
 
 	return (
 		<form onSubmit={handleOnSubmit}>
 			<div className="grid grid-cols-1 gap-y-4">
 				<div className="col-span-full">
-					<label
-						htmlFor="todo"
-						className="block text-sm font-medium leading-6 text-gray-800"
-					>
-						New Task
-					</label>
-					<Input name="todo" />
+					<Label htmlFor="todo">New Task</Label>
+					<Input name="todo" placeholder="What do you wanna do?" />
+				</div>
+
+				<div className="col-span-full">
+					<Label htmlFor="date">Date</Label>
+					<Input type="date" name="date" />
+				</div>
+
+				<div className="col-span-full">
+					<Label htmlFor="time">Time</Label>
+					<Input type="time" name="time" />
 				</div>
 				<div className="my-4 border-t border-slate-200"></div>
 				<div className="col-span-full flex items-center justify-end gap-x-4">
-					<button
-						type="button"
-						className="px-3 py-2 font-medium leading-6 text-gray-900 border rounded-md"
-					>
-						Cancel
-					</button>
 					<Button type="submit">Save</Button>
 				</div>
 			</div>
--- components/Input.jsx
+++ components/Input.jsx
@@ -2,12 +2,11 @@ export default function Input({
 	type = "text",
 	name,
 	classname = "",
-	placeholder = "Add task",
+	placeholder = "",
 }) {
 	return (
 		<input
 			type={type}
-			id={name}
 			name={name}
 			className={`block mt-2 w-full rounded-md border-0 py-2.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:leading-6 ${classname}`}
 			placeholder={placeholder}
